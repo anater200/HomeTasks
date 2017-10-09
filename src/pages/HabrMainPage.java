cd91dd299c19f4e4040d2df287b8f5b9f9223d06
anater200 <anater200@gmail.com>
2017-10-09 17:48:35 +0300

    new tests fix

--- src/pages/HabrMainPage.java
+++ src/pages/HabrMainPage.java
@@ -5,10 +5,12 @@ import elements.Label;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 
+
+
 public class HabrMainPage {
     private Button loginButton = new Button(By.xpath("//*[@id='login']"));
-    private Button userProfileButton = new Button(By.xpath("//*[@class='dropdown dropdown_user dropdown_active']"));
-    private Label userProfileLabel = new Label(By.xpath("//div[@class='dropdown-container dropdown-container_white']/..//span[contains(text().'testbestdb')]"));
+    private Button userProfileButton = new Button(By.xpath("//*[@class='dropdown dropdown_user']"));
+    private Label quitLabel = new Label(By.linkText("Выйти"));
 
 
     public HabrLoginPage goToLoginPage() {
@@ -22,6 +24,6 @@ public class HabrMainPage {
 
     }
     public void verifyUserNickname() {
-        Assert.assertTrue(userProfileLabel.isPresent());
+        Assert.assertTrue(quitLabel.isPresent());
     }
 }
